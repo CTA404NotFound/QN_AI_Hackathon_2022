871e06ca5ef68707b6b5547a3405da474d78652d
MarkusKhoa <dangkhoa20006@gmail.com>
2022-08-04 15:21:43 +0700

    Modified model code

--- models.py
+++ models.py
@@ -1,20 +1,24 @@
-from transformers import BertPreTrainedModel, RobertaModel, BertModel
+from transformers import AutoTokenizer, AutoModel
+
 import torch
+import torch.nn as nn
 
 
-class PhoBertAspectClassification(BertPreTrainedModel):
+class PhoBertSentimentClassification(nn.Module):
     """
-        PhoBert Pre-trained model for aspect classification
+        PhoBert Pre-trained model for aspect and polarity score classification
     """
 
-    def __init__(self, config):
-        super().__init__(config)
-        self.roberta = RobertaModel(config)
-        self.num_classes = config.num_classes
-        self.dense = torch.nn.Linear(config.hidden_size * 4, self.num_labels)
+    def __init__(self, n_labels):
+        super(PhoBertSentimentClassification, self).__init__(n_labels)
+        self.phobert = AutoModel.from_pretrained("vinai/phobert-base")
+        self.dropout = nn.Dropout(p = 0.3)
+        self.n_labels = n_labels
+        self.dense = torch.nn.Linear(self.phobert.hidden_size * 4, self.n_labels)
 
     def forward(self, input_ids, attention_mask):
-        outputs = self.roberta(input_ids, attention_mask=attention_mask)
+        outputs = self.phobert(input_ids, attention_mask=attention_mask,
+                               return_dict = False)
         cls_output = torch.cat((outputs[2][-1][:, 0, ...],
                                 outputs[2][-2][:, 0, ...],
                                 outputs[2][-3][:, 0, ...],
