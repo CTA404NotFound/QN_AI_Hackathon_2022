572af3cf1cce747b3df92c1abb7aa1c615f171b8
MarkusKhoa <dangkhoa20006@gmail.com>
2022-08-08 22:51:25 +0700

    Update evaluation code

--- eval.py
+++ eval.py
@@ -66,6 +66,6 @@ def eval_metrics(aspect_idx, prediction, label):
     return aspect_f1_score, aspect_r2_score
 
 
-# pred = [[1, 0, 3, 4, 2, 0], [0, 2, 2, 5, 1, 0], [2, 3, 1, 5, 4, 0]]
-# gt = [[1, 1, 3, 5, 3, 1], [0, 0, 3, 4, 0, 0], [2, 1, 1, 5, 3, 0]]
-# print(pred[2][0])
+pred = [[1, 0, 3, 4, 2, 0], [0, 2, 2, 5, 1, 0], [2, 3, 1, 5, 4, 0]]
+gt = [[1, 1, 3, 5, 3, 1], [0, 0, 3, 4, 0, 0], [2, 1, 1, 5, 3, 0]]
+print(eval_metrics(5, pred, gt))
