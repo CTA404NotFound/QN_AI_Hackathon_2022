697a15d3aa7591f40b385af86882010eb859ce00
MarkusKhoa <dangkhoa20006@gmail.com>
2022-08-16 15:20:08 +0700

    Modify model

--- Final_Folder/hackathon-example-submit/model.py
+++ Final_Folder/hackathon-example-submit/model.py
@@ -1,41 +1,55 @@
+from transformers import AutoModel,BertPreTrainedModel
+import torch
 import torch.nn as nn
+import time
+from transformers import RobertaModel, RobertaConfig, AutoConfig, AutoTokenizer
+from transformers.models.roberta.modeling_roberta import RobertaPreTrainedModel
 import torch
 
-from transformers import AutoModel
 from config import *
 
-class PhoBertSentimentClassification(nn.Module):
+class MaskClassifier(RobertaPreTrainedModel):
+    """Custom PhoBert model
+
+    Args:
+        RobertaPreTrainedModel (object): Pre-trained model
     """
-        PhoBert Pre-trained model for aspect and polarity score classification
-    """
+    def __init__(self, config):
+        super().__init__(config=config)
+        self.bert = RobertaModel(config)
+        # self.bert = BertModel(config)
+        self.dropout = nn.Dropout(p=0.1)
+        self.classifier1 = nn.Linear(config.hidden_size, config.hidden_size)
+        self.classifier = nn.Linear(config.hidden_size, 30)
 
-    def __init__(self):
-        super(PhoBertSentimentClassification, self).__init__()
-        self.bert = AutoModel.from_pretrained(BERT_NAME)
-        self.dropout = nn.Dropout(p = 0.1)
+        nn.init.normal_(self.classifier.weight, std=0.02)
+        nn.init.normal_(self.classifier.bias, 0)
 
-        # self.n_labels = n_labels
-        self.fc = nn.Linear(self.bert.config.hidden_size, 30)
-        nn.init.normal_(self.fc.weight, std = 0.02)
-        nn.init.normal_(self.fc.bias, 0)
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+    ):
 
-    def forward(self, input_ids, attention_mask):
         last_hidden_state, output = self.bert(
-            input_ids=input_ids,
-            attention_mask=attention_mask,
-            return_dict=False # Dropout will errors if without this
+        input_ids=input_ids,
+        attention_mask=attention_mask,
+        return_dict=False # Dropout will errors if without this
         )
         x = self.dropout(output)
-        x = self.fc(x)
+        x = self.classifier1(x)
+        x = torch.tanh(x)
+        x = self.dropout(x)
+        x = self.classifier(x)
         return x
 
 model_path = MODEL_FILE_NAME
-model = PhoBertSentimentClassification()
-model.load_state_dict(
-            torch.load(model_path,
-                       map_location=torch.device('cpu')),
-            strict=False)
-model.to(torch.device(DEVICE))
+model = MaskClassifier.from_pretrained("D:/AI Camp/QN_AI_Hackathon_2022/Final_Folder/hackathon-example-submit/PhoBert_train/")
+# model.load_state_dict(
+#             torch.load(model_path,
+#                        map_location=torch.device('cpu')),
+#             strict=False)
+# model.to(torch.device(DEVICE))
 
 def predict(input_ids, input_mask):
     global model
